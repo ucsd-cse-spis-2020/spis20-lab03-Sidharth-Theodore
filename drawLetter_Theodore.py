249ffedac582d72d555f07930499f9c27dd87d1f
Theodore Hui <tchui@ucsd.edu>
2020-08-10 20:48:54 +0000

    Added docstring

--- drawLetter_Theodore.py
+++ drawLetter_Theodore.py
@@ -1,5 +1,7 @@
+#Theodore Hui, draws letter T
 import turtle
 def drawT(theTurtle):
+    '''Takes a turtle, uses it to draw letter T'''
     theTurtle.pd()
     theTurtle.forward(100)
     theTurtle.right(180)
